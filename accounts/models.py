83a894be8c2ea2ae6501cb35641146d76b2e4cf4
ApolloNick <nkostrov7@gmail.com>
2022-04-20 14:25:50 +0300

    Created flake8

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(to='auth.User',
+                                on_delete=models.CASCADE,
+                                related_name='profile')
+
+    @classmethod
+    def generate_profile(cls):
+        for user in User.objects.all():
+            try:
+                user.profile
+            except ValueError:
+                profile = Profile()
+                profile.user = user
+                profile.save()
